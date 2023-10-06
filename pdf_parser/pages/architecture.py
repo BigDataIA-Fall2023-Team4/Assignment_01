14e83e6e565f44bc078eaad645f84b8017ff8340
Naman Gupta <naman2602@gmail.com>
2023-10-06 02:48:31 -0400

    arch

--- pdf_parser/pages/architecture.py
+++ pdf_parser/pages/architecture.py
@@ -3,5 +3,5 @@ from PIL import Image
 
 
 def diagram():
-    image = Image.open('diagrams/architecture.png')
+    image = Image.open('architecture.png')
     st.image(image, caption='Architecture Diagram')
